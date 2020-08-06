c0e8aea802d3e19dcce338b008495615c916ce4f
Admin-Ishrat <shourov100@yahoo.com>
2020-08-06 00:58:32 -0700

    Gradle setup

---
+++ src/main/java/ca/cmpt213/a4/onlinehangman/Application.java
@@ -0,0 +1,17 @@
+package ca.cmpt213.a4.onlinehangman;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+ * Application class starts the SpringBoot Tomcat web server on the localhost:8080
+ * This class calls the Controller class to access the endpoints.
+ *
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+
+    }
+}
